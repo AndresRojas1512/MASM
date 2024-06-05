5ddb679cc3b9fb89521b9316dd20b8faca29a7d3
AndresRojas1512 <andresrojasfcc@gmail.com>
2024-06-05 13:06:01 +0300

    L03 : Comment to sample 3

--- lab_03/samples/lab3_3/lr03_3.asm
+++ lab_03/samples/lab3_3/lr03_3.asm
@@ -1,33 +1,33 @@
-SD1 SEGMENT para public 'DATA'
-	S1 db 'Y'
-	db 65535 - 2 dup (0)
+SD1 SEGMENT para public 'DATA'		; сегмент данных SD1
+	S1 db 'Y'						; объавление байта S1 и инициализация значением 'Y'
+	db 65535 - 2 dup (0)			; заполнение остальной части сегмента нулями
 SD1 ENDS
 
-SD2 SEGMENT para public 'DATA'
-	S2 db 'E'
-	db 65535 - 2 dup (0)
+SD2 SEGMENT para public 'DATA'		; определение сегмента данных SD2
+	S2 db 'E'						; объявление байта S2 и инициализация значением 'E'
+	db 65535 - 2 dup (0)			; заполнение остальной части сегмента нулями
 SD2 ENDS
 
-SD3 SEGMENT para public 'DATA'
-	S3 db 'S'
-	db 65535 - 2 dup (0)
+SD3 SEGMENT para public 'DATA'		; определение сегмента данных SD3
+	S3 db 'S'						; объявление байта S2 и инициализация значением 'E'
+	db 65535 - 2 dup (0)			; заполнение остальной части сегмента нулями
 SD3 ENDS
 
-CSEG SEGMENT para public 'CODE'
+CSEG SEGMENT para public 'CODE'		; определение сегмента кода
 	assume CS:CSEG, DS:SD1
-output:
-	mov ah, 2
-	int 21h
-	mov dl, 13
-	int 21h
-	mov dl, 10
-	int 21h
+output:								; метка начала процедуры вывода
+	mov ah, 2						; функция 2 для вывода символа
+	int 21h							; прерывание для вывода
+	mov dl, 13						; перевод каретки в начало
+	int 21h							; врерывание для перевода
+	mov dl, 10						; перевод строки
+	int 21h							; прерывание для перевода
 	ret
 main:
-	mov ax, SD1
-	mov ds, ax
-	mov dl, S1
-	call output
+	mov ax, SD1						; загрузка адрес сегмента в AX
+	mov ds, ax						; устовка DS для указания на сегмент SD1
+	mov dl, S1						; загрузка значения Y в DL
+	call output						; вывод
 assume DS:SD2
 	mov ax, SD2
 	mov ds, ax
@@ -39,7 +39,7 @@ assume DS:SD3
 	mov dl, S3
 	call output
 	
-	mov ax, 4c00h
+	mov ax, 4c00h					; завершение программы
 	int 21h
 CSEG ENDS
 END main
